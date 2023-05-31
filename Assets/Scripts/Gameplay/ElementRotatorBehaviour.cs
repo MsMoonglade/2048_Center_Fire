8e2467464876bfd423af54f319c2bc2068da8501
MsMoonglade <moonglade95@gmail.com>
2023-05-31 11:36:42 +0200

    base rotator pos

--- Assets/Scripts/Gameplay/ElementRotatorBehaviour.cs
+++ Assets/Scripts/Gameplay/ElementRotatorBehaviour.cs
@@ -1,18 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static MoreMountains.FeedbacksForThirdParty.MMFeedbackHaptics;
+using UnityEngine.AI;
 
 public class ElementRotatorBehaviour : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public float rotator_Speed;
+    public float rotator_Radius;
+  
+    private void Awake()
     {
-        
+        /*
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            float angle = i * Mathf.PI * 2f / transform.childCount;
+            Vector3 newPos = new Vector3(Mathf.Cos(angle) * radius, 0.5f , Mathf.Sin(angle) * radius);
+            transform.GetChild(i).transform.localPosition = newPos;
+        }
+        */
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            float angle = i * Mathf.PI * 2f / transform.childCount;
+            Vector3 newPos = new Vector3(Mathf.Cos(angle) * rotator_Radius, 0.5f, Mathf.Sin(angle) * rotator_Radius);
+            transform.GetChild(i).transform.localPosition = newPos;
+        }
+
+        transform.RotateAround(transform.position, transform.up, rotator_Speed * Time.deltaTime);
     }
 }
